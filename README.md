c8446496ca82f75198e38c0f75597791025e9290
Yelizaveta Petrych <yelizaveta_petrych@epam.com>
2022-02-08 14:11:47 +0200

    Script updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Cosplay UA Moderator Bot
 
 * Ban users with duplicated messages for 7 days. 
-* Duplicated message - is repeated 2+ times in 7 days, contains 4+ words
+* Duplicated message - is repeated 2+ times in 7 days, contains 5+ words
--- src/index.js
+++ src/index.js
@@ -2,24 +2,32 @@
 
 const TelegramBot = require('node-telegram-bot-api')
 const ms = require('ms')
+const fs = require('fs')
 
-const { TELEGRAM_BOT_TOKEN } = process.env
+const { TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_OWNER } = process.env
 
 if (!TELEGRAM_BOT_TOKEN) {
   console.error('Seems like you forgot to pass Telegram Bot Token. I can not proceed...')
   process.exit(1)
 }
 
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true })
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true})
 
-const messages = {}
+console.log('### Telegram Bot is up and running! ### ')
+
 const restrictionTime = '7 days'
+const maxWhitespacesAllowed = 4
+const logFilePath = './log.txt'
+
+const messages = getOldMessages()
 
 bot.on('message', (msg) => {
-  if (msg.text.split(' ').length < 5) return
+  if (msg.text.split(' ').length <= maxWhitespacesAllowed) return
 
-  const userId = msg.from.id
-  const chatId = msg.chat.id
+  const { from, chat, text, date } = msg
+
+  const userId = from.id
+  const chatId = chat.id
 
   const startRestrictionDate = Math.round((Date.now() - ms(restrictionTime)) / 1000)
   let messagesByUserId = messages[chatId] && messages[chatId][userId] || []
@@ -32,18 +40,47 @@ bot.on('message', (msg) => {
     }
   })
 
-  if (!msg.from.is_bot && foundMessages.includes(msg.text)) {
-    bot.deleteMessage(chatId, msg.message_id)
-    bot.restrictChatMember(chatId, userId,  {
-      until_date: Math.round((Date.now() + ms(restrictionTime)) / 1000),
-    })
+  if (isRegularUser && foundMessages.includes(text)) {
+    handleDuplicatedMessage(msg, chatId, userId)
+
+    saveNewLog(messages)
+    return
   }
 
   messages[chatId] = messages[chatId] || {}
   messages[chatId][userId] = messagesByUserId || []
   messages[chatId][userId].push({
-    date: msg.date,
-    text: msg.text,
+    date,
+    text,
   })
+
+  saveNewLog(messages)
 })
 
+function handleDuplicatedMessage(msg, chatId, userId) {
+  console.log('duplicated message', msg)
+
+  try {
+    bot.deleteMessage(chatId, msg.message_id)
+    bot.restrictChatMember(chatId, userId, {
+      until_date: Math.round((Date.now() + ms(restrictionTime)) / 1000),
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function getOldMessages() {
+  const data = fs.readFileSync(logFilePath, { encoding:'utf8', flag:'r' })
+
+  return data ? JSON.parse(data) : {}
+}
+
+function saveNewLog(messages) {
+  fs.writeFileSync(logFilePath, JSON.stringify(messages), { encoding:'utf8' })
+}
+
+function isRegularUser(from) {
+  return !from.is_bot
+  && from.username !== TELEGRAM_BOT_OWNER
+}
